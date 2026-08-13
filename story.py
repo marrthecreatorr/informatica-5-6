993e2816ca65df8616f089294d1eb464ec6167af
marrthecreatorr <marlenoemi14@gmail.com>
2026-08-13 15:41:31 +0000

    lower, upper

--- story.py
+++ story.py
@@ -14,10 +14,10 @@ def main():
     # print("Hello", end=" ")
     # print(planet)
 
-    name = input("What is your name? ")
-    color = input("Tell me your color: ")
-    adjetive = input("Give me a adjetive:")
-    goal = input("What is your goal? ")
+    name = input("What is your name? ").strip().title()
+    color = input("Tell me your color: ").strip().lower()
+    adjetive = input("Give me a adjetive:").strip().lower()
+    goal = input("What is your goal? ").strip().lower()
 
     print(f"Hello, {name}!")
     print()
@@ -25,6 +25,17 @@ def main():
     print("This is your story:")
     print(f"At dawn the sky turned {color}, and the air felt {adjetive}. I decided today {goal}.")
 
+
+    name = input("What is your name? ").strip().upper()
+    color = input("Tell me your color: ").strip().upper()
+    adjetive = input("Give me a adjetive:").strip().upper()
+    goal = input("What is your goal? ").strip().upper()
+
+    print(f"Hello, {name.strip()}!")
+    print()
+
+    print("This is your story:")
+    print(f"At dawn the sky turned {color}, and the air felt {adjetive}. I decided today {goal}.".strip().upper())
 
 if __name__ == "__main__":
     main()
