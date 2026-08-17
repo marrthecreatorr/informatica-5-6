f2720004dc6985236862c94dc0449774ca78577f
marrthecreatorr <marlenoemi14@gmail.com>
2026-08-17 14:59:27 +0000

    rectangle.py

---
+++ rectangle.py
@@ -0,0 +1,13 @@
+def main():
+    w = int(input("Width: "))
+    print("o" * w)
+    print("o" * w)
+    print("o" * w)
+    print("o" * w)
+    print("o" * w)
+
+
+
+if __name__ == "__main__":
+    main()
+
