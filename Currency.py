64695836ce1b0e9ec6caafbdf5312c5a609d3745
marrthecreatorr <marlenoemi14@gmail.com>
2026-08-18 20:19:39 +0000

    currency.py

---
+++ Currency.py
@@ -0,0 +1,9 @@
+def main()
+    p = float(input("What do you have left in pesos? "))
+    s = float(input("What do you have left in soles? "))
+    r = float(input("What do you have left in reales "))
+
+    usd = ((p / 0.0032) + (s / 0.30) +(r / 0.19) r)50)
+
+    if __name__=="__main__":
+    main()
