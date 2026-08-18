52427db7f0ba27df03bb62f14a44e1e7996ee379
marrthecreatorr <marlenoemi14@gmail.com>
2026-08-18 19:59:22 +0000

    rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,4 +1,6 @@
 def main():
+
+    l = 5
     w = int(input("Width: "))
     print("o" * w)
     print("o" * w)
@@ -6,6 +8,14 @@ def main():
     print("o" * w)
     print("o" * w)
 
+    p = (2 * l) + (2 * w)
+    print("perimeter:", p)
+
+    a = (l * w)
+    print("Area", a)
+
+    d = ((1**2) + (w**2) )**0.5
+    print("Diagonal:" , d)
 
 
 if __name__ == "__main__":
