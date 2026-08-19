be729eab05391594c5f4dbca1da91b75e3fc5b26
marrthecreatorr <marlenoemi14@gmail.com>
2026-08-19 18:00:28 +0000

    update-variable.py

---
+++ update-variable.py
@@ -0,0 +1,35 @@
+def main():
+    #Augmented Assignment Operator
+    score=10
+    score= score+ 10
+    score += 10
+
+    print(score)
+
+    # Substraction Assignment Operator
+    # x -= y
+    grade = 100
+    grade -= 15
+
+    print(grade)
+
+    # Multiplication Assignment Operator
+    balance = 10
+    balance *= 4
+
+    print(balance)
+
+    #Division Assignment Operator
+    x = 10
+    x /= 5
+    print(x)
+
+    #Modulus Assignment Operator
+    a = 10
+    b = 4
+    a %= b
+    print(a)
+
+
+if __name__ == "__main__":
+    main()
