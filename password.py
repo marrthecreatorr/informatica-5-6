282440e8b67141b89997970abdbe3df2ca9afbfe
marrthecreatorr <marlenoemi14@gmail.com>
2026-08-24 14:53:31 +0000

    password

---
+++ password.py
@@ -0,0 +1,14 @@
+def main():
+
+
+    password = "goldfishsuper08"
+    attempt = input("What is your password? ")
+
+    if attempt == password:
+        print("yeii you remember!!!")
+    else:
+        print("blowing up computer😥")
+
+
+if __name__ == "__main__":
+    main()
