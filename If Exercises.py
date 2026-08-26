7b91d6b8c3335e752cfe5340fbbf19086d128243
marrthecreatorr <marlenoemi14@gmail.com>
2026-08-26 18:10:33 +0000

    if excercises.py

---
+++ If Exercises.py
@@ -0,0 +1,32 @@
+def main():
+
+#easy
+    # num = int(input("Type a integer number:" ))
+
+    # if num  < 0:
+       # print(num * -1)
+    # else:
+       # print(num)
+#medium
+   # num1 = int(input("Give me a number:"))
+   # num2 = int(input("Give me another number:"))
+   # calc = input("Do you want to add, sub or multiply?")
+
+   # if calc == "add":
+    #    print(num1 + num2)
+   # elif calc == "sub":
+    #    print(num1 - num2)
+   # elif calc == "multiply":
+    #    print(num1 * num2)
+   # else:
+    #    print("Blowing up ccomputer")
+
+hard
+    oper = input("Enter your arimetical operation:")
+    
+
+
+
+if __name__=="__main__":
+    main()
+
