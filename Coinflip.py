3e6e70470462d20c09d0e5942e16e60a8adcee88
marrthecreatorr <marlenoemi14@gmail.com>
2026-09-01 21:49:15 +0000

    coinflip.py

--- Coinflip.py
+++ Coinflip.py
@@ -1,16 +1,20 @@
+import random
 def main():
+    coin = ["heads","tails"]
+    attempts = 3
+    while attempts > 0:
+        flip = random.choice(coin)
+        guess = input("Heads or Tails?: ").strip().lower()
 
-    guess = print("Heads or Tails?: ").lower()
-    outcome = random.raint(1,2)
+        print("The coin landed on", flip)
 
-    if guess == heads:
-        if outcome == 1:
-        print("You guessed {guess}, the coin was {outcome}"
-
-    elif outcome == 2:
-        outcome = tails
-        print("You )
-
+        if guess == flip:
+            print("Winner")
+            break
+        else:
+            print("Loser")
+            attempts -= 1
+            print("Attempts left:", attempts)
 
 if __name__ == "__main__":
     main()
