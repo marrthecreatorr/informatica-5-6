c4f98d365427090fee8cd9f78181b9cf668496a6
marrthecreatorr <marlenoemi14@gmail.com>
2026-09-01 20:18:14 +0000

    coinflip.py

---
+++ Coinflip.py
@@ -0,0 +1,16 @@
+def main():
+
+    guess = print("Heads or Tails?: ").lower()
+    outcome = random.raint(1,2)
+
+    if guess == heads:
+        if outcome == 1:
+        print("You guessed {guess}, the coin was {outcome}"
+
+    elif outcome == 2:
+        outcome = tails
+        print("You )
+
+
+if __name__ == "__main__":
+    main()
