bef43253ab9c38f78fd11eff8a027f83b106dae6
marrthecreatorr <marlenoemi14@gmail.com>
2026-09-02 18:06:09 +0000

    roadtrip.py

---
+++ roadtrip.py
@@ -0,0 +1,15 @@
+def main():
+    answer = ""
+    followup = ""
+
+    while answer != "Yes!":
+        answer = input("Are we there yet? ").title().strip()
+        if answer == "Yes":
+            followup = input("Really? ").title().strip()
+        if followup == "Yes":
+            break
+
+
+    print("We are here!")
+if __name__ == "__main__":
+    main()
