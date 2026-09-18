a32b12b996487d7b46f2d070540b80205eb39e97
marrthecreatorr <marlenoemi14@gmail.com>
2026-09-18 20:18:50 +0000

    for-intro.py

---
+++ for-intro.py
@@ -0,0 +1,12 @@
+def main ():
+    doctrine = ["faith in Jesus Christ", "repentence", "Baptism in water", "Confemation", "Enduring the end"]
+
+    list = 1
+    for hi in doctrine:
+        print(f"{list}. {hi}")
+        list += 1
+
+
+if __name__ == "__main__":
+    main()
+
