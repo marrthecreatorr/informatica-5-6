a402b79655793daa1db22885fbad9aa8e1ca9267
marrthecreatorr <marlenoemi14@gmail.com>
2026-09-18 21:39:44 +0000

    to-do.py

--- to-do.py
+++ to-do.py
@@ -1,13 +1,23 @@
 def main():
-    tasks = [] #Empty list
+    tasks = [] #empty list
 
     while True:
         print(f"Tasks to do: {len(tasks)}")
         print(tasks)
-        command = input("what do you want to do? (add, complete, exit): ")
+
+        command = input("what do you want to do? (add, complete, prioritize, exit): ")
         if command == "add":
             new_task = input("Enter new task: ")
             tasks.append(new_task)
+        elif command == "Complete":
+            comp_task = input("Which task did you complete?: ")
+            task.remove(comp_task)
+        elif command == "prioritize":
+            pri_task = input("What task do you want to prioritize?: ")
+            tasks.insert(0,pri_task)
+        elif command == "exit":
+            print("ByeBye")
+            break
 
 if __name__ == "__main__":
     main()
